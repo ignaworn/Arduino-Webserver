02e730811c64f07ea4dbc67e80933b2fad59ffe3
Ignacio Worn <igna.worn@gmail.com>
2013-12-27 21:31:02 -0300

    Create the function object despite CONTROL_PLACARD is enabled or not. If it is disabled, just return with no values.

--- firmware/ControlPlacad.ino
+++ firmware/ControlPlacad.ino
@@ -25,48 +25,50 @@
     // Timer Active
     const   byte* CPTimer_Active = &Parameters[1];
 
+#endif
 
-    void ControlPlacard() {
+void ControlPlacard() {
+    #ifndef CONTROL_PLACARD
+        return;
+    #endif
 
-      bool SA2 = SA1;
-      bool SB2 = SB1;
-      SA1 = digitalRead(CPI[0]);
-      SB1 = digitalRead(CPI[1]);
+    bool SA2 = SA1;
+    bool SB2 = SB1;
+    SA1 = digitalRead(CPI[0]);
+    SB1 = digitalRead(CPI[1]);
 
-      if ( *CPTimer_Active and CPTimer > 0 and (millis() - CPTimer) >= CPTimeout )   {
-          if(debug) Serial << endl << "Control Placard - Timeout" << endl;
-          SetPin (CPO[0], LOW);
-          SetPin (CPO[1], LOW);
-      }
-
-      if ( SA1 != SA2) {
-        if ( SA1 ) {
-          if(debug) Serial << endl << "Control Placard - Door closed" << endl;
-          SetPin (CPO[0], LOW);
-          SetPin (CPO[1], LOW);
-        }
-        else {
-          if(debug) Serial << endl << "Control Placard - Door opened" << endl;
-          SetPin(CPO[0], HIGH);
-          if (*CPTimer_Active) {
-            if(debug) Serial << "Control Placard - Started Timer" << endl;
-            CPTimer = millis();
-          }
-        }
-      }
-
-      if (*CPTimer_Active and CPTimer > 0 and !digitalRead(CPO[0])) {
-          if(debug) Serial << "Control Placard - Timer reset" << endl;
-          CPTimer = 0;
-      }
-
-      if ( SB1 != SB2 ) {
-        if ( SB1 and !SA1 and digitalRead(CPO[0]) and !digitalRead(CPO[1]) ) {
-          if(debug) Serial << endl << "Control Placard - Door fully opened" << endl;
-          SetPin(CPO[1], HIGH);
-        }
-      }
-
+    if ( *CPTimer_Active and CPTimer > 0 and (millis() - CPTimer) >= CPTimeout )   {
+        if(debug) Serial << endl << "Control Placard - Timeout" << endl;
+        SetPin (CPO[0], LOW);
+        SetPin (CPO[1], LOW);
     }
 
-#endif
+    if ( SA1 != SA2) {
+        if ( SA1 ) {
+        if(debug) Serial << endl << "Control Placard - Door closed" << endl;
+            SetPin (CPO[0], LOW);
+            SetPin (CPO[1], LOW);
+        }
+        else {
+            if(debug) Serial << endl << "Control Placard - Door opened" << endl;
+            SetPin(CPO[0], HIGH);
+            if (*CPTimer_Active) {
+                if(debug) Serial << "Control Placard - Started Timer" << endl;
+                CPTimer = millis();
+            }
+        }
+    }
+
+    if (*CPTimer_Active and CPTimer > 0 and !digitalRead(CPO[0])) {
+        if(debug) Serial << "Control Placard - Timer reset" << endl;
+        CPTimer = 0;
+    }
+
+    if ( SB1 != SB2 ) {
+        if ( SB1 and !SA1 and digitalRead(CPO[0]) and !digitalRead(CPO[1]) ) {
+            if(debug) Serial << endl << "Control Placard - Door fully opened" << endl;
+            SetPin(CPO[1], HIGH);
+        }
+    }
+
+}
