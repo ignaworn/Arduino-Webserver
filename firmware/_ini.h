8d760c55e4d488cbd93ce03ca7b6fa83b88b380f
Ignacio Worn <igna.worn@gmail.com>
2013-12-30 02:25:28 -0300

    _ini.h: Changed include semicolon to brackets

--- firmware/_ini.h
+++ firmware/_ini.h
@@ -2,9 +2,9 @@
     #define INI_H
 
     #if defined(ARDUINO) && ARDUINO >= 100
-        #include "Arduino.h"
+        #include <Arduino.h>
     #else
-        #include "WProgram.h"
+        #include <WProgram.h>
     #endif
 
     // Define the device ID
