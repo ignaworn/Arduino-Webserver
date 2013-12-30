d2e06a1a4c80a6cd3fec7660ac6291ef222f5205
Ignacio Worn <igna.worn@gmail.com>
2013-12-30 17:10:54 -0300

    _alarms.ino: Do not load _alarms if NTP_TIMER Flag is not active

--- firmware/_alarms.ino
+++ firmware/_alarms.ino
@@ -1,9 +1,11 @@
-// Everyday at 7:30am
-void MorningAlarm() {
-    // Master Parameter 0 ON
-    SetParameters(0,0x01);
-    // SlaveLed Parameter 0 ON
-    #ifdef SLAVE_LED
-        Slave.SendData('P',0x00,0x01);
-    #endif
-}
+#ifdef NTP_TIMER
+    // Everyday at 7:30am
+    void MorningAlarm() {
+        // Master Parameter 0 ON
+        SetParameters(0,0x01);
+        // SlaveLed Parameter 0 ON
+        #ifdef SLAVE_LED
+            Slave.SendData('P',0x00,0x01);
+        #endif
+    }
+#endif
